7b01d91d8819f38aa15de376ac781b10dd144abc
Nicholas Anjewierden <nicholasanjewierden0@gmail.com>
2018-05-04 15:19:20 -0700

    messeduplastcommit

--- shelfie/src/routes.js
+++ shelfie/src/routes.js
@@ -3,5 +3,5 @@ import {Switch, Route} from 'react-router-dom'
 import Dashboard from './Component/Dashboard/Dashboard'
 
 export default(
-    <Switch><Route path='dashboard' component={Dashboard}/></Switch>
+    <Switch><Route path='/dashboard' component={Dashboard}/></Switch>
 )
